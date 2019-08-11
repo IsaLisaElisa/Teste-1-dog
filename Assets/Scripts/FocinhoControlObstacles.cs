0f6ce66d337d148d735308bfb6fbbf076cce34fd
IsaLisaElisa <elisamirandas22015@gmail.com>
2019-08-11 09:26:44 -0300

    Arrumei script FocinhoControlObstacles

--- Assets/Scripts/FocinhoControlObstacles.cs
+++ Assets/Scripts/FocinhoControlObstacles.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FocinhoControlObstacles : MonoBehaviour
 {
-     public GameObject player;
+    public GameObject player;
 
     MoveToRef script;
     Animator anim;
