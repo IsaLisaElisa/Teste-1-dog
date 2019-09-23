ae50172439a73889dad0ceeb538334ef59ed35ee
IsaLisaElisa <elisamirandas22015@gmail.com>
2019-09-23 09:25:37 -0300

    Fiz os paineis, mas os botoes ainda nao funcionam

---
+++ Assets/Scripts/MainLevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainLevelManager : MonoBehaviour
+{
+    void Update()
+    {
+        void ShowCJPanel() {
+        uiComponents.CJPanel.panel.SetActive(true);
+    }
+        void ShowCreditosPanel() {
+        uiComponents.CreditosPanel.panel.SetActive(true);
+    }
+    }
+}
