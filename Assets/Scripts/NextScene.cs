44bf38060f4506c86765e71f393fa9609a075475
Nelio Alves <acenelio@gmail.com>
2019-12-02 09:11:58 -0300

    Preparacao para publicacao

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public float waitTime = 3f;
+    public string nextScene = "Main";
+    
+
+    void Start()
+    {
+        StartCoroutine (Next());
+    }
+
+    IEnumerator Next (){
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(nextScene);
+    }
+}
